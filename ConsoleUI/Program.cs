450ba71e27f3844f550802dc48e969feefcc6164
huysuzcadı huysuzcadı <sevim23112013@gmail.com>
2021-02-21 01:03:52 +0300

    İlk Projem

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleUI
     {   
         static void Main(string[] args)
         {
+
             ProductManager productManager = new ProductManager(new InMemoryProductDal());
 
             foreach (var product in productManager.GetAll())
